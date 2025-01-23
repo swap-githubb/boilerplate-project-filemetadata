f4b638a59a5340732e00764fb5189c212ace5e0e
Swapnil Shiv <shivswapnil531@gmail.com>
2025-01-23 06:56:30 +0000

    committed

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
-
 app.post('/api/fileanalyse', multer().single('upfile'), (req, res) => {
   res.json({
       name: req.file.originalname,
@@ -16,10 +15,6 @@ app.post('/api/fileanalyse', multer().single('upfile'), (req, res) => {
       size: req.file.size
   });
 });
-
-
-
-
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)

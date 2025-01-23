83740c2ed1ccae127492537dd295af5c6f9142dd
Swapnil Shiv <shivswapnil531@gmail.com>
2025-01-23 06:49:43 +0000

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
-
+require('dotenv').config();
+var multer=require('multer');
 var app = express();
 
 app.use(cors());
@@ -9,6 +9,14 @@ app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', multer().single('upfile'), (req, res) => {
+  res.json({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size
+  });
 });
 
 
